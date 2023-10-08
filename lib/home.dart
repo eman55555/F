12fcfdba767338370c56dcec41950d5347c4f0f6
eman hamad <emanhamad55555@gmail.com>
2023-10-08 02:45:00 +0200

    Added Day_10

--- lib/home.dart
+++ lib/home.dart
@@ -1,13 +1,9 @@
-import 'package:day_8/login.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-  var password;
-  var username;
-  var data;
-  HomeScreen({this.password, this.username, this.data, super.key});
+  const HomeScreen({super.key, required this.title});
 
-  //  x = x;  this.x  //ali = ahmed // this.ahmed
+  final String title;
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -16,62 +12,101 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    // When a BuildContext is used from a StatefulWidget, the mounted property
-    // must be checked after an asynchronous gap.
+    List<Card> _buildGridCards(int count) {
+      List<Card> cards = List.generate(
+        count,
+        (int index) {
+          return Card(
+            clipBehavior: Clip.antiAlias,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                AspectRatio(
+                    aspectRatio: 18.0 / 11.0,
+                    child: Icon(
+                      Icons.diamond_sharp,
+                      size: 40,
+                      color: Colors.brown[700],
+                    )
+                    // Image.asset('assets/diamond.png'),
+                    ),
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Title'),
+                      SizedBox(height: 8.0),
+                      Text('Secondary Text'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      );
+      return cards;
+    }
 
     return Scaffold(
-      appBar: AppBar(title: Text("Second"), automaticallyImplyLeading: false),
-      body: Center(
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "${widget.data}",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "UserName :",
-                  style: TextStyle(
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.deepPurple),
-                ),
-                Text("${widget.username}"),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "  Password : ",
-                  style: TextStyle(
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.deepPurple),
-                ),
-                Text("${widget.password}"),
-              ],
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                  ScaffoldMessenger.of(context)
-                    ..removeCurrentSnackBar()
-                    ..showSnackBar(SnackBar(content: Text('Done !')));
-                },
-                child: Icon(Icons.backpack))
-          ],
+      //backgroundColor: Color.fromARGB(255, 233, 226, 230),
+      appBar: AppBar(
+        elevation: 0,
+        //backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.menu,
+            semanticLabel: 'menu',
+          ),
+          onPressed: () {
+            print('Menu app bar');
+          },
         ),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.search,
+              semanticLabel: 'search',
+            ),
+            onPressed: () {
+              print('Search app bar');
+            },
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.tune,
+              semanticLabel: 'filter',
+            ),
+            onPressed: () {
+              print('Filter app bar');
+            },
+          ),
+        ],
       ),
+      body: GridView.count(
+          crossAxisCount: 2,
+          padding: const EdgeInsets.all(16.0),
+          childAspectRatio: 8.0 / 9.0,
+          children: _buildGridCards(12) // Replace
+          ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   items: const <BottomNavigationBarItem>[
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.call),
+      //       label: 'Calls',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.camera),
+      //       label: 'Camera',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.chat),
+      //       label: 'Chats',
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,19 @@
-import 'package:day_8/login.dart';
+import 'package:day_8/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: const LoginScreen(title: "Login Page"),
+    home: const HomeScreen(title: 'SHRINE'),
     debugShowCheckedModeBanner: false,
     theme: ThemeData(
-      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      //  colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       useMaterial3: true,
     ),
   ));
 }
-/*local & global keys :*/
+// to change launcher icon :
 
-// There are four types of keys in Flutter, which are as follows:
-
-// Value key: It stores the alphanumeric value of the widget.
-// Object key: It stores complex objects in which multiple objects have the same values, such as date of birth or name. It usually would be a custom class.
-// Unique key: It stores every widget state as unique. It is easy to identify them when we need to recall them.
-// Global key: It allows developers to access the data of one widget inside another in your application. It can help the developers but can also destroy the state management. It is a very controversial key as better state management options are available. It can be helpful in form validation situations where we need to validate some common data.
-
-/* if - else */
-
-// Use the ternary operator
-// condition ? Text("True") : null,
-// Use If or For statements or spread operators in collections
-// children: [
-//   ...manyItems,
-//   oneItem,
-//   if(canIKickIt)
-//     ...kickTheCan
-//   for (item in items)
-//     Text(item)
-
-/* loop */
-// Two alternatives :
-
-// final children = <Widget>[];
-// for (var i = 0; i < 10; i++) {
-//   children.add(new ListTile());
-// }
-// return new ListView(
-//   children: children,
-// );
-// or
-
-// return new ListView(
-//   children: new List.generate(10, (index) => new ListTile()),
-// );
-
-/* switch case */
-// switch (expression) {
-//   case ONE:
-//     {
-//       statement(s);
-//     }
-//   break;
-
-//   case TWO:
-//     {
-//       statement(s);
-//     }
-//   break;
-
-//   default:
-//     {
-//       statement(s);
-//     }
-// }
+// 1- flutter pub run flutter_launcher_icons
+//2-Create a folder called asset in the root directory
+//3- Add flutter_icons inside the pubspec.yaml 
+//4- flutter pub get
